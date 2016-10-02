7f61a12190c15048cc272deb93bcc5db0b7518ee
correct-horse-battery-staple <pandasmoothie@gmail.com>
2016-10-02 18:31:44 -0400

    Add files via upload
    Home Screen scene and associated scripts and prefabs

---
+++ ChangeNotificationBubble.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeNotificationBubble : MonoBehaviour {
+
+	public Image notificationBubble;
+
+	protected static bool hasBubble;
+
+	public void start(){
+		hasBubble = false;
+	}
+
+
+}
