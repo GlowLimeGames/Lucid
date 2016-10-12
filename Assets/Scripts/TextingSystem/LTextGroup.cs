b401a2ef51593e0693ade30cf18b764dab4cbb06
Isaiah Mann <contact@isaiahmann.com>
2016-10-12 10:41:59 -0400

    Adding basic project structure and basic text parsing system

---
+++ Assets/Scripts/TextingSystem/LTextGroup.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Represents a group of texts in the mobile game Lucid
+ */
+
+[System.Serializable]
+public class LTextGroup {
+	public string GroupID;
+	public LText[] Texts;
+	public int Count {
+		get {
+			return Texts.Length;
+		}
+	}
+	public LText this[int index] {
+		get {
+			if (index >= 0 && index < Count) {
+				return Texts[index];
+			} else {
+				return null;
+			}
+		}
+	}
+}
