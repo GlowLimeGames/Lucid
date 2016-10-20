89b60ad772c32b3177b40e3436786754f618af37
Isaiah Mann <contact@isaiahmann.com>
2016-10-20 15:25:52 -0400

    Updating files from binary to force text

---
+++ Assets/Scripts/Tutorial/ITutorialController.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Template for the tutorial
+ */
+
+public interface ITutorialController {
+	void PlayTutorial (string tutorialName);	
+}
---
+++ Assets/Scripts/Tutorial/LTutorialController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LTutorialController : Controller, ITutorialController {
+
+	public void PlayTutorial (string tutorialName) {
+		Debug.Log("Playing tutorial " + tutorialName);
+	}
+
+	protected override void SetReferences () {
+		// Nothing
+	}
+
+	protected override void FetchReferences () {
+		// Nothing
+	}
+
+	protected override void HandleNamedEvent (string eventName) {
+		// Nothing
+	}
+
+	protected override void CleanupReferences () {
+		// Nothing
+	}
+}
