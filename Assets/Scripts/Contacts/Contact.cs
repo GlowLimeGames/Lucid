3f0927cf77ec25451cdf92308b7eaf121cbcb2e3
Joseph Gillen <joegillen@ymail.com>
2016-10-20 17:07:42 -0400

    Adding Contact UI Skeleton

---
+++ Assets/Scripts/Contacts/Contact.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Author: Joseph Gillen
+ * Initial Date: 20th October 2016
+ * Description: This class is used to show contact details on contact screen
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Contact : MonoBehaviour {
+
+    private GameObject ContactImage, ContactName;
+    LContact instanceOfContact = new LContact();
+    // For each contact call the contact creation method
+    // Temperary function for hard coded values
+    public LContact CreateContact(LContact contact)
+    {
+        instanceOfContact.ContactID = contact.ContactID;
+        instanceOfContact.ContactName = contact.ContactName;
+        instanceOfContact.NumberOfMessagesRecieved = contact.NumberOfMessagesRecieved;
+        instanceOfContact.NumberOfMessagsSent = contact.NumberOfMessagsSent;
+        instanceOfContact.SpriteContactImage = contact.SpriteContactImage;
+        instanceOfContact.BoolIsMessageUnread = contact.BoolIsMessageUnread;
+        instanceOfContact.BoolIsContact = contact.BoolIsContact;
+        Debug.Log(ContactImage.name);
+        AssignNameAndImage();
+        return instanceOfContact;
+    }
+    // Giving the contact a name and image
+    public void AssignNameAndImage()
+    {
+        ContactImage = gameObject.transform.GetChild(0).gameObject;
+        ContactName = gameObject.transform.GetChild(1).gameObject;
+        ContactImage.GetComponent<Image>().sprite = instanceOfContact.SpriteContactImage;
+        ContactName.GetComponent<Text>().text = instanceOfContact.ContactName;
+    }
+    // Trigger Message Loading
+    public void LoadMessageUI()
+    {
+        Debug.Log("Ohh wee look at me");
+    }
+}
---
+++ Assets/Scripts/Contacts/ContactScreen.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Author: Joseph Gillen
+ * Initial Date: 20th October 2016
+ * Description: This class spawns the contacts
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ContactScreen : MonoBehaviour {
+
+    public GameObject Contact;
+    public Vector3 spawnPosition;
+    // HARD CODED HORROR
+    LContact[] arrayOfContacts = new LContact[3];
+	// Use this for initialization
+	void Start () {
+        spawnPosition = gameObject.transform.position;
+        // HARD CODED HORROR
+        for (int i = 0; i < 3; i++)
+        {
+            arrayOfContacts[i] = new LContact();
+            arrayOfContacts[i].ContactID = "0" + i;
+            arrayOfContacts[i].ContactName = "Name" + i;
+            arrayOfContacts[i].NumberOfMessagesRecieved = 0;
+            arrayOfContacts[i].NumberOfMessagsSent = 0;
+            arrayOfContacts[i].SpriteContactImage = null;
+            arrayOfContacts[i].BoolIsMessageUnread = false;
+            arrayOfContacts[i].BoolIsContact = true;
+        }
+        // HARD CODED HORROR
+        for (int i = 0; i < 3; i++)
+        {
+            InstatiateContact(i);
+            spawnPosition.y -= 50;
+        }
+    }
+    void InstatiateContact(int i)
+    {
+        // Create a contact with the above information
+        GameObject aContact = Instantiate(Contact, spawnPosition, Quaternion.identity) as GameObject;
+        aContact.transform.parent = gameObject.transform;
+        aContact.GetComponent<Contact>().AssignNameAndImage();
+        aContact.GetComponent<Contact>().CreateContact(arrayOfContacts[i]);
+    }
+}
