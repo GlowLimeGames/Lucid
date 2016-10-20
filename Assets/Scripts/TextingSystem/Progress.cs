238fc0f153fd1bfe4508ca5fab353c9d4d3fd96e
Samuel Stallop <sstallop19@amherst.edu>
2016-10-20 15:37:48 -0400

    adding progress script WIP

---
+++ Assets/Scripts/TextingSystem/Progress.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Progress : MonoBehaviour{
+
+    //LTextGroup texts;
+    int ID;
+
+	void Start () 
+	{
+        ID = 1;
+        /*
+
+        LTextGroup textGroup = JsonUtility.FromJson<LTextGroup>(Resources.Load<TextAsset>("Texting/Example").text);
+
+        */
+    }
+
+
+
+		
+
+
+	void Update()
+	{
+
+	
+	}	
+
+    void ChooseA()
+    {
+        /*
+        foreach(LText text in texts)
+        {
+            if((text.ID).Equals(ID + "") && (text.choice).Equals("A"))
+            {
+                ProgressResponse pResponse = new ProgressResponse(text);
+            }
+
+
+        }
+        */
+    }
+
+    void ChooseB()
+    {
+        /*
+        foreach(LText text in texts)
+        {
+            if((text.ID).Equals(ID + "") && (text.choice).Equals("B"))
+            {
+                
+            }
+
+
+        }
+        */
+    }
+
+    void ChooseC()
+    {
+        /*
+       foreach(LText text in texts)
+       {
+           if((text.ID).Equals(ID + "") && (text.choice).Equals("C"))
+           {
+                
+           }
+
+
+        }
+        */
+    }
+
+
+
+}
