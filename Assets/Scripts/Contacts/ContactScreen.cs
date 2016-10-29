c1a69d0718ea4bb75bb8e0435251d7ffd38771b5
Kevin Wang <kewang19@amherst.edu>
2016-10-28 22:01:12 -0400

    Messaging UI

--- Assets/Scripts/Contacts/ContactScreen.cs
+++ Assets/Scripts/Contacts/ContactScreen.cs
@@ -11,8 +11,8 @@ public class ContactScreen : LScreenController {
 	LCharacterController character;
     public GameObject Contact;
     public Vector3 spawnPosition;
-	public float YOffset = -200;
-	public float ContactYSpacing = 50;
+	public float YOffset = -85;
+	public float ContactYSpacing = 40;
 	public float ContactSize = 2;
 
 	// I have redeemed their hard coded souls - Isaiah
@@ -20,7 +20,9 @@ public class ContactScreen : LScreenController {
 
 	protected override void SetReferences () {
 		base.SetReferences ();
-		spawnPosition = gameObject.transform.position + Vector3.down * YOffset;
+		Vector3 temp = gameObject.transform.position;
+		temp.y = temp.y * -1;
+		spawnPosition = Vector3.down * YOffset + temp/ContactSize;
 	}
 
 	protected override void FetchReferences () {
@@ -39,8 +41,12 @@ public class ContactScreen : LScreenController {
 	void InstatiateContact(LContact contact) {
         // Create a contact with the above information
         GameObject aContact = Instantiate(Contact, spawnPosition, Quaternion.identity) as GameObject;
+		//Debug.Log ("spawn position: " + spawnPosition);
+		//Debug.Log ("contact position: " + aContact.transform.position);
 		spawnPosition.y -= ContactYSpacing;
+
 		aContact.transform.SetParent (gameObject.transform);
+		//aContact.transform.Translate (aContact.transform.position);
         aContact.GetComponent<Contact>().AssignNameAndImage();
 		aContact.GetComponent<Contact>().CreateContact(contact, ContactSize);
     }
