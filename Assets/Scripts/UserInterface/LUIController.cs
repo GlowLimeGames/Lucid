050b899821e368b76466999ebd8182164150d858
David Tuttle <dtuttle@umass.edu>
2016-10-30 16:35:10 -0400

    Popups spawnable from LUIController

--- Assets/Scripts/UserInterface/LUIController.cs
+++ Assets/Scripts/UserInterface/LUIController.cs
@@ -3,7 +3,11 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LUIController : SingletonController<LUIController>, IUIController {
+	public GameObject popup;
 	public void LoadScene (string sceneName) {
 		SceneManager.LoadScene(sceneName);
+	}
+	public void spawnPopup(){
+			Instantiate (popup);
 	}
 }
