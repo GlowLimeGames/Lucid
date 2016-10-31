1f34213f8cd8ce3bac9967c2934b8d747c2c3148
Isaiah Mann <contact@isaiahmann.com>
2016-10-31 14:52:05 -0400

    Fixing pop up appearing and compile errors

--- Assets/Scripts/UserInterface/LUIController.cs
+++ Assets/Scripts/UserInterface/LUIController.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
+﻿/*
+ * Authors: Isaiah Mann, David Tuttle
+ * Description: 
+ */
+
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LUIController : SingletonController<LUIController>, IUIController {
 	public GameObject popup;
+
 	public void LoadScene (string sceneName) {
 		SceneManager.LoadScene(sceneName);
 	}
+
 	public void spawnPopup(string bodyTxt, string btnTxt){
-			Instantiate (popup);
-			popup.setTxt(bodyTxt);
-			popup.setBtnTxt(btnTxt);
+		Popup popup = Instantiate (this.popup).GetComponent<Popup>();
+		popup.setText(bodyTxt);
+		popup.setBtnText(btnTxt);
 	}
 }
--- Assets/Scripts/UserInterface/Popup.cs
+++ Assets/Scripts/UserInterface/Popup.cs
@@ -6,29 +6,27 @@ public class Popup : MonoBehaviour {
 	public GameObject textComp;
 	public Text btnText;
 	public Button btn;
+
 	// Use this for initialization
 	void Start () {
 		messageText = textComp.GetComponent<Text> ();
 		//Debugging purposes
 		btn.onClick.AddListener(dismiss);
 		btnText =btn.GetComponentInChildren<Text> ();
-		setText ("");
-		setBtnText ("");
-
 	}
 
 	public void setText(string str){
 		if (str == "") {
 			messageText.text = "This is a test of the popup system";
 		} else {
-			messageText.text = "str";
+			messageText.text = str;
 		}
 	}
 	public void setBtnText(string str){
 		if (str == "") {
 			btnText.text = "dismiss";
 		} else {
-			btnText.text = "str";
+			btnText.text = str;
 		}
 	}
 
