d8215f04dbab36ce36afc16ca2b4a310e277ab67
Kevin Wang <kewang19@amherst.edu>
2016-11-02 14:56:21 -0400

    no message

--- Assets/Scripts/UserInterface/ButtonResponse.cs
+++ Assets/Scripts/UserInterface/ButtonResponse.cs
@@ -6,30 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class ButtonResponse : MonoBehaviour {
 
-	protected string[] apps; //contains the Scene names for each app
-
-	//debug functionality iterating number in notification bubbles on click
-	//private int count;
-
-	//debug functionality changing the time with the buttons
-	//public Text timePanel;
+	protected string[] apps;
 
 	void Start(){
-	//	count = 1;
 	}
 
 	public void buttonClick(int index){
-
-		//Debug.Log (index);
 		SceneManager.LoadSceneAsync ("App Template");//	apps [index]);
 		Scene newScene = SceneManager.GetSceneByName ("App Template");
-		/*
-		Debug.Log (newScene.IsValid());
-		if(!(newScene.IsValid() || newScene.isLoaded)){
-			SceneManager.LoadSceneAsync("App Template");//	apps [index]);
-			newScene = SceneManager.GetSceneByName("App Template");
-		}
-		*/
 		SceneManager.SetActiveScene (newScene);//apps [index]);
 	}
 }
