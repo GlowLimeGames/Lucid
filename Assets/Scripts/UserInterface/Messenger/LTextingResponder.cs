a1c5f8a5bc5fad4076c1609801c3f4cb4ce10c7e
Isaiah Mann <contact@isaiahmann.com>
2016-11-04 11:40:47 -0400

    Working on setting up response buttons

--- Assets/Scripts/UserInterface/Messenger/LTextingResponder.cs
+++ Assets/Scripts/UserInterface/Messenger/LTextingResponder.cs
@@ -4,13 +4,42 @@
  */
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LTextingResponder : LUIPanel {
+	protected LMessengerScreenController controller;
 	protected ToggleController toggle;
+
+	LUILabledButton[] responseButtons;
+	LText[] currentResponses;
+	public bool HasResponses {
+		get {
+			return currentResponses != null;
+		}
+	}
 
 	protected override void SetReferences () {
 		base.SetReferences ();
 		toggle = GetComponentInChildren<ToggleController>();
+		controller = GetComponentInParent<LMessengerScreenController>();
+		responseButtons = GetComponentsInChildren<LUILabledButton>();
 	}
+
+	public void SetResponses (params LText[] responses) {
+		this.currentResponses = responses;
+		for (int i = 0; i < responseButtons.Length; i++) {
+			if (i < responses.Length) {
+				int index = i;
+				responseButtons[i].Show();
+				responseButtons[i].Set(responses[i].Body, 
+					delegate {
+						controller.SendText(responses[index]);
+					});
+			} else {
+				responseButtons[i].Hide();
+			}
+		}
+	}
+		
 }
